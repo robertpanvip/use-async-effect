c5e9f9f97b14640ab12c1d600a92ebc42e9a67d7
pan <robertpanvip@163.com>
2023-03-24 21:52:42 +0800

    feat:丰富细节

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# use-async-effect
-@hooks-plus/usePromiseEffect
+
+**可以接受async编程方式的副作用函数**
+
+📦 **Installation**
+``` javascript
+npm install @hooks-plus/use-async-effect
+```
+🔨 **Usage**
+> Note: 可以像React.useEffect一样使用也可以在useAsyncEffect使用async函数 不过promise中需要返回一个作用域给cleanup回收副作用
+
+see demo
+
--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,12 @@ function useAsyncEffect<R>(
     cleanup: CleanupCallback<R>,
     deps?: DependencyList
 ): void;
-
+/**
+ * 可以接受async编程方式的副作用函数
+ * @param effect 可以像React.useEffect一样使用也可以使用promise 不过promise中需要返回一个作用域给cleanup回收副作用
+ * @param cleanup 回收副作用的函数
+ * @param deps 依赖
+ */
 function useAsyncEffect<R>(
     effect: PromiseEffectCallback<R>,
     cleanup: unknown,
